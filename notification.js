be82c11f2dbc7b5621b7633af83b70b9c0685d43
Evgeny Fedorenko <evgeny.fedorenko@constant.obninsk.ru>
2022-06-10 16:14:10 +0300

    push

---
+++ notification.js
@@ -0,0 +1,15 @@
+
+// Initialize Firebase - https://firebase.google.com/docs/web/setup
+var config = {
+  apiKey: "AIzaSyDP5jy66Zlhq1ZgP5i63rYftxmsV_j6SaM",
+  authDomain: "ticket-9dd8c.firebaseapp.com",
+  projectId: "ticket-9dd8c",
+  storageBucket: "ticket-9dd8c.appspot.com",
+  messagingSenderId: "91734333449",
+  appId: "1:91734333449:web:666903eb97efae02b200d8",
+  measurementId: "G-FWSFYHQBS3"
+};
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+
